65b7b20a771e7c10f323abca6c0d0bf0f6df10f8
Irek Iwanca <iiwanca@gmail.com>
2022-11-30 19:01:32 +0100

    .eslintrc.json added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import { Nav, NavbarContainer } from "./Navbar.elements";
+import React, {useState} from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
+import {IconContext} from "react-icons/lib";
+import {
+ Nav,
+ NavbarContainer,
+ NavLogo,
+ NavIcon,
+ MobileIcon,
+ NavMenu,
+ NavItem,
+ NavLinks,
+} from "./Navbar.elements";
 
 // eslint-disable-next-line no-undef
 
-
 const Navbar = () => {
-    return (
-        <Nav>
-            <NavbarContainer>
+ const [click, setClick] = useState(false);
 
-            </NavbarContainer>
+ const handleClick = () => setClick(!click);
 
-        </Nav>
-    );
+ return (
+  <>
+   <IconContext.Provider value={{color: '#fff'}}>
+    <Nav>
+     <NavbarContainer>
+      <NavLogo to='/'>
+       <NavIcon/>
+      </NavLogo>
+      <MobileIcon onClick={handleClick}>
+       {click ? <FaTimes/> : <FaBars/>}
+      </MobileIcon>
+      <NavMenu onClick={handleClick} click={click}>
+       <NavItem>
+        <NavLinks to='/'>
+         Home
+        </NavLinks>
+       </NavItem>
+       <NavItem>
+        <NavLinks to='/student'>
+         Student area
+        </NavLinks>
+       </NavItem>
+       <NavItem>
+        <NavLinks to='/teacher'>
+         Teacher area
+        </NavLinks>
+       </NavItem>
+      </NavMenu>
+
+     </NavbarContainer>
+    </Nav>
+   </IconContext.Provider>
+  </>
+ )
+  ;
 };
 
 export default Navbar;
