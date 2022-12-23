aa4cb7f7f8c68bb6d9c2f51573828bd8cb1cac40
Irek Iwanca <iiwanca@gmail.com>
2022-12-23 09:19:52 +0100

    eslint config amended

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  // eslint-disable-next-line react/jsx-filename-extension
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    // eslint-disable-next-line react/jsx-filename-extension
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
